31c9e138ad9d38671ea4a219d6b910267124c784
Abhimanyu Gupta <120638939+abhimanyu345@users.noreply.github.com>
2025-07-13 03:00:29 +0530

    [947] [Time Beats: 45.06%] [Memory Beats: 41.01%] - LeetPush

--- Most-Stones-Removed-with-Same-Row-or-Column.cpp
+++ Most-Stones-Removed-with-Same-Row-or-Column.cpp
@@ -1,78 +1,69 @@
 class DisjointSet {
-    vector<int> rank, parent, size;
+vector<int>parent, rank;
 public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
+    DisjointSet (int n){
+        parent.resize(n+1, 0);
+        rank.resize(n+1, 0);
+        for(int i=0; i<=n; i++)
             parent[i] = i;
-            size[i] = 1;
-        }
     }
 
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
+    int findParent(int x)
+    {
+        if(parent[x]==x)
+            return x;
+        else
+            return parent[x] = findParent(parent[x]);
     }
 
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
+    void Union(int x, int y)
+    {
+        int par_x = findParent(x);
+        int par_y = findParent(y);
+        if(rank[par_x] > rank[par_y])
+        {
+            parent[par_y] = par_x;
         }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
+        else if(rank[par_y] > rank[par_x])
+        {
+            parent[par_x] = par_y;
         }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        else
+        {
+            parent[par_x] = par_y;
+            rank[par_y]++;
         }
     }
 };
+
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size();
+        
         int maxRow = 0;
         int maxCol = 0;
-        for (auto it : stones) {
+        for(auto it: stones)
+        {
             maxRow = max(maxRow, it[0]);
             maxCol = max(maxCol, it[1]);
         }
+
         DisjointSet ds(maxRow + maxCol + 1);
         unordered_map<int, int> stoneNodes;
         for (auto it : stones) {
             int nodeRow = it[0];
             int nodeCol = it[1] + maxRow + 1;
-            ds.unionBySize(nodeRow, nodeCol);
+            ds.Union(nodeRow, nodeCol);
             stoneNodes[nodeRow] = 1;
             stoneNodes[nodeCol] = 1;
         }
 
         int cnt = 0;
         for (auto it : stoneNodes) {
-            if (ds.findUPar(it.first) == it.first) {
+            if (ds.findParent(it.first) == it.first) {
                 cnt++;
             }
         }
-        return n - cnt;
+        return stones.size() - cnt;
     }
 };
