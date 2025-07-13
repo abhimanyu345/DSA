303956e4e5376295aca3aaeec5b938bdf851212e
Abhimanyu Gupta <120638939+abhimanyu345@users.noreply.github.com>
2025-07-13 23:51:03 +0530

    [3] [Time Beats: 35.92%] [Memory Beats: 36.26%] - LeetPush

---
+++ Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> window;
+        int left = 0, right = 0, maxLen = 0;
+
+        while (right < s.length()) {
+            if (!window.count(s[right])) {
+                window.insert(s[right]);
+                maxLen = max(maxLen, right - left + 1);
+                right++;
+            } else {
+                window.erase(s[left]);
+                left++;
+            }
+        }
+
+        return maxLen;
+    }
+};
